dc2f9a264ee9e707b79e6a0f12a60c9a4013f13a
souleid <souleid@gmail.com>
2026-01-02 09:28:44 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

--- C/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.c
+++ C/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.c
@@ -5,14 +5,5 @@
 // numbers_len은 배열 numbers의 길이입니다.
 double solution(int numbers[], size_t numbers_len) {
     double answer = 0;
-    
-    if(numbers_len <= 0) return answer;
-    
-    for( int i = 0; i<numbers_len; i++) {
-        answer += numbers[i];
-    }
-    
-    answer = answer / numbers_len;
-    
     return answer;
 }
