21bcdff1fc50469318864cc73ca32a2e94597fbe
souleid <souleid@gmail.com>
2026-02-09 09:16:46 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    for(const string str : strlist) {
+        answer.push_back(str.length());
+    }
+    return answer;
+}
