ec038495757325210d03ad3af1717730f19eabef
souleid <souleid@gmail.com>
2026-02-27 15:52:52 +0900

    Revert "fixed directory path and source file name ASCII"
    This reverts commit 8bce16c2ca260f63f7baeb72332e1e88c61b887b.

--- C/Programmers/0/120817. Array_Average/Array_Average.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-// numbers_len은 배열 numbers의 길이입니다.
-double solution(int numbers[], size_t numbers_len) {
-    double answer = 0;
-    return answer;
-}
--- C/Programmers/0/120820. Problem/Problem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int age) {
-    int answer = 0;
-    answer = 2022 - age + 1;
-    return answer;
-}
